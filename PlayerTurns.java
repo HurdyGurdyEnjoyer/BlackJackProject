504e1d3c075eb725daa4b9306e2326bb49868af4
Gavin C <162391140+DiedToRemoval@users.noreply.github.com>
2024-04-12 17:55:08 -0400

    Update PlayerTurns.java
    added new player actions and added the base of a function to loop through an individual player's turn.

--- PlayerTurns.java
+++ PlayerTurns.java
@@ -5,55 +5,117 @@
  * to be called from a driver class of some kind
  */
 
-package BlackJackTurns;
-
 class PlayerTurn 
 {//PlayerTurn class start
 	
-	//Stuff to do during a turn
+	//Stuff to do during a turn || will also need to grab the player array from the game class
 	/*
 	 * placing bet
 	 * hit (take as many cards as want until over 21)
+	 * double 
+	 * surrender
 	 * Stay (do nothing and end turn)
+	 * (maybe) split
 	 */
 	
+	//loop through an individual player's turn to hit, stay, double, surrender, or split. 
 	
-	
-	public int placeBet(/*betAmount*/)
-	{//placeBet method start
-		try
+	public void handleTurn()
+	{//handleTurn method start
+		
+		//variables
+		int selection;
+		String optTable = "1) Hit\n2) Double\n3) Split\n4) Stay\n5) Surrender";
+		
+		do
 		{
-			//check if the betted amount is less than or equal to the total amount of money available  
-			//if ((betAmount + listOfPlayers.get(playerID).getBet) < listOfPlayers.get(playerID).getMoney)
+			//Prompting for, getting, and parsing user input
+			System.out.print("please choose between 1-5\n" + 
+					optTable +
+					">>> ");
+			selection = Integer.parseInt(keyIn.nextLine());
+			
+			switch(selection)
 			{
-				//listOfPlayers.get(playerID).setBet(betAmount + listOfPlayers.get(playerID).getBet);
-				//listOfPlayers.get(playerID).setFunds(betAmount);
-				return 0;
+			case 1:
+				playerHit();
+			break;
+			case 2:
+				playerDouble();
+			break;
+			case 3:
+				//Split
+			break;
+			case 5:
+				playerSurrender();
+			break;
+			default:
+				//stay
 			}
-			//else
-			//{
-				//return -2; //return overdraft from funds
-		    //}
-		}catch (Exception e)
-		{
-			return -1;//return error
-		}
+				
+			
+			
+			
+			
+		} while(!(selection == 2 || selection == 4 || selection == 5));
+		
+	}//handleTurn method end
+	
+	public void placeBet(double betAmount)
+	{//placeBet method start
+		
+		listOfPlayers.get(playerID).setBet(betAmount + listOfPlayers.get(playerID).getBettedAmount);
+		listOfPlayers.get(playerID).withdrawBalance(betAmount);
 		
 	}//placeBet method end
 	
-	public int playerHit(/*int playerID*/)
+	public void playerHit(/*int playerID*/)
 	{//playerHit method start
-		try
-		{
-			//get a card from the dealer, add that card to the player's hand and add that card's points to the player's point total
-			//cardID = dealer.drawCard();
-			//listOfPlayers.get(playerID).addHand(cardID);
-			//listOfPlayers.get(playerID).addPoints(Deck.cards.getRank(cardID));
-			return 0; //no error
-		}catch (Exception e)
-		{
-			return -1; //return error
-		}
+	
+		//get a card from the dealer, add that card to the player's hand and add that card's points to the player's point total
+
+		String cardRank, cardSuit;
+		//draw card from dealer
+		
+		listOfPlayers.get(playerID).addToHand(cardRank, cardSuit);
+		
 	}//playerHit method end
 	
+	public void playerDouble()
+	{//playerDouble method start
+		
+		String cardRank, cardSuit;
+		double betAmount = listOfPlayers.get(playerID).getBettedAmount()
+		//draw card from dealer
+		
+		//adding card to player's hand
+		listOfPlayers.get(playerID).addToHand(cardRank, cardSuit);
+		
+		//withdrawing money from player balance and doubling their bet
+		listOfPlayers.get(playerID).withdrawBalance(betAmount);
+		listOfPlayers.get(playerID).addBettedAmount(betAmount);
+		
+	}//playerDouble method end
+	
+	public void playerSplit()
+	{//playerSplit method start
+		
+		//create a second player that is linked to the original player
+		//this "player"'s betted amount is equal to the inital amount betted
+		
+	}//playerSplit method end
+	
+	public void playerSurrender()
+	{//playerSurrender method start
+		Player curPlayer = new listOfPlayers.get(playerID);
+		double halfBet = (curPlayer.getBettedAmount() / 2);
+		
+		//halving bet and adding funds to balance
+		curPlayer.setBettedAmount(halfBet);
+		curPlayer.setBalance(curPlayer.getBalance() + halfBet);
+		
+		//clearing whole hand and deleting hand size
+		curPlayer.clearHand();
+		
+	}//playerSurrender method end
 }//PlayerTurn class end
