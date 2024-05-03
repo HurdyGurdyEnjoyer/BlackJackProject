36a73d2472f9a427d0bfdec0eac741326915fcb4
BrendanMalone22 <160981584+BrendanMalone22@users.noreply.github.com>
2024-05-02 21:22:58 -0400

    Create 213 Players.java

---
+++ 213 Players.java
@@ -0,0 +1,30 @@
+/**
+ * assigns hand to player
+ */
+
+/**
+ * @author maloneb97994
+ *
+ */
+public class Players {
+	
+	//player manyPlayers <arraylist>;
+	
+	//Arraylist of singleCard
+	
+	int SumCards;
+	
+	
+	
+	
+	/**
+	 * Dealer is always the last player in the arraylist when adding players insert new player as player 0.
+	 * 
+	 */
+	// add player
+
+		
+	
+
+	
+}
