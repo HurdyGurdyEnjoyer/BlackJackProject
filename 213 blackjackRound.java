2919b6d227ad3a70ba95633620453e36de46abc6
BrendanMalone22 <160981584+BrendanMalone22@users.noreply.github.com>
2024-05-02 21:24:32 -0400

    Create 213 blackjackRound.java

---
+++ 213 blackjackRound.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+
+/**
+ * @author maloneb97994
+ *
+ */
+public class blackjackRound {
+	// 
+	Players playing = new Players();  
+	deck cardsforRound = new deck();
+
+	
+	
+	
+	/**
+	 * @param playing
+	 */
+	public blackjackRound(Players playing) {
+		super();
+		this.playing = playing;
+	}
+
+	private void round()
+	{
+		cardsforRound.shuffle();
+		//player bet
+		//disrupt cards to Players(...).singleCard
+		// Loop for Players turn.
+		// if last player use deal turn
+			// use dealer rules
+		
+	}
+	
+	// fx take card and provide to each player
+	
+	// double  - minor
+	// stand
+	// hit
+	// split  - minor
+	// surround  - minor
+	
+	
+	
+	//Determine Outcome
+	
+}
